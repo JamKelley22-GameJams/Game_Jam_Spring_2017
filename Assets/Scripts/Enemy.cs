5910f32af9b9fea6f3454940fccc1b3c43ed348a
Jameel <jkelley@iastate.edu>
2017-01-21 06:34:44 -0600

    pathing to thingie...

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,19 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour {
 	public int health;
 	public float moveSpeed;
+	public float posX;
+	public float posY;
+	public float cumPosX = 0, cumPosY = 0;
 
+	Transform player;
+
+	private Vector3 moveDir;
+
+
+	void Awake() {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		moveToPlayer ();
 	}
 
 	public void takeDamage(int damage) {
@@ -25,6 +38,27 @@ public class Enemy : MonoBehaviour {
 
 	void die() {
 		Destroy (this.gameObject);
+	}
+
+	public void moveToPlayer() {
+
+		transform.position = Vector3.MoveTowards (transform.position,player.transform.position,moveSpeed * Time.deltaTime);
+
+		/*
+
+		posX = Time.deltaTime * moveSpeed;
+		posY = Time.deltaTime * moveSpeed;
+
+		cumPosX = this.gameObject.transform.position.x + posX;
+		cumPosY = this.gameObject.transform.position.y + posY; 
+		//moveDir = player.position - this.gameObject.transform.position;
+		moveDir = new Vector3 (cumPosX,cumPosY,0);
+		//this.gameObject.transform.LookAt (player.transform.position,Vector3.left);
+
+		this.gameObject.transform.position = moveDir;
+		*/
+
+
 	}
 }
 
