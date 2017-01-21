8253705f226d366d9873360204c41ac42db5e10a
Jacob Stair <Jacob Stair>
2017-01-20 21:16:07 -0600

    tell me that shit

---
+++ Assets/Scripts/Aim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour {
+
+	public int[] mousePos = { 0, 0 }; //variable for the mouse position
+	const int X = 0; //
+	const int Y = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
