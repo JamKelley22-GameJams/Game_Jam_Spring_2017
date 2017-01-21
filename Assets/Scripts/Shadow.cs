fb15361c2b7a4763958f46d7c28cbde9688a781c
Jacob Stair <Jacob Stair>
2017-01-21 12:43:27 -0600

    glow bois

---
+++ Assets/Scripts/Shadow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shadow : MonoBehaviour {
+
+	public float yOff;
+	public GameObject parent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3 (parent.transform.position.x,
+			parent.transform.position.y + yOff,
+			parent.transform.position.z);
+	}
+}
