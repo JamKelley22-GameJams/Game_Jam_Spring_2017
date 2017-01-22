689b0acb93fe0be94df6366e84f3fba600cef112
Jameel <jkelley@iastate.edu>
2017-01-21 23:07:12 -0600

    weapon swaping finished :)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,12 +18,73 @@ public class Player : MonoBehaviour {
 	public GameObject beamGunS;
 	public GameObject weirdGunS;
 
+	public GameObject pickUpWeapon;
+
+	public bool inSpace = false;
+
+	private string otherTag;
+
 	void Start () {
 		
 	}
 
 	void Update () {
-		
+		if(Input.GetMouseButtonDown(1) && inSpace) {
+			if(otherTag.Equals("Beam_Gun")) {
+				Debug.Log ("Found Beam Gun");
+				beamGun.SetActive(true);
+				waveGun.SetActive(false);
+				weirdGun.SetActive(false);
+
+				if(hasWaveGun) {
+					waveGunS.SetActive(true);
+				}
+				if(hasWeirdGun) {
+					weirdGunS.SetActive(true);
+				}
+				beamGunS.SetActive(false);
+
+				hasBeamGun = true;
+				hasWaveGun = false;
+				hasWeirdGun = false;
+			}
+			if(otherTag.Equals("Wave_Gun")) {
+				Debug.Log ("Found Wave Gun");
+				beamGun.SetActive(false);
+				waveGun.SetActive(true);
+				weirdGun.SetActive(false);
+
+				if(hasBeamGun) {
+					beamGunS.SetActive(true);
+				}
+				if(hasWeirdGun) {
+					weirdGunS.SetActive(true);
+				}
+				waveGunS.SetActive(false);
+
+				hasBeamGun = false;
+				hasWaveGun = true;
+				hasWeirdGun = false;
+			}
+			if(otherTag.Equals("Weird_Gun")) {
+				Debug.Log ("Found Weird Gun");
+				beamGun.SetActive(false);
+				waveGun.SetActive(false);
+				weirdGun.SetActive(true);
+
+				if(hasBeamGun) {
+					beamGunS.SetActive(true);
+				}
+				if(hasWaveGun) {
+					waveGunS.SetActive(true);
+				}
+				weirdGunS.SetActive(false);
+
+				hasBeamGun = false;
+				hasWaveGun = false;
+				hasWeirdGun = true;
+			}
+		}
 	}
 
 	public void takeDamage(float damage) {//argggggggghhhhhhh
@@ -36,40 +97,20 @@ public class Player : MonoBehaviour {
 
 	}
 
-	void OnTriggerEnter2D(Collider2D other) {
-		if(other.gameObject.tag.Equals("Beam Gun")) {
-			Debug.Log ("Found Beam Gun");
-			beamGun.SetActive(true);
-			waveGun.SetActive(false);
-			weirdGun.SetActive(false);
-			hasBeamGun = true;
-			hasWaveGun = false;
-			hasWeirdGun = false;
-
-			if(hasWaveGun) {
-				
-			}
-			if(hasWeirdGun) {
-
-			}
-		}
-		if(other.gameObject.tag.Equals("Wave Gun")) {
-			Debug.Log ("Found Wave Gun");
-			beamGun.SetActive(false);
-			waveGun.SetActive(true);
-			weirdGun.SetActive(false);
-			hasBeamGun = false;
-			hasWaveGun = true;
-			hasWeirdGun = false;
-		}
-		if(other.gameObject.tag.Equals("Weird Gun")) {
-			Debug.Log ("Found Weird Gun");
-			beamGun.SetActive(false);
-			waveGun.SetActive(false);
-			weirdGun.SetActive(true);
-			hasBeamGun = false;
-			hasWaveGun = false;
-			hasWeirdGun = true;
-		}
+	void OnTriggerExit2D() {
+		inSpace = false;
 	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		//Instantiate (pickUpWeapon, this.gameObject.transform.position, Quaternion.identity);
+		if(other.gameObject.tag.Equals("Beam_Gun") || other.gameObject.tag.Equals("Wave_Gun") || other.gameObject.tag.Equals("Weird_Gun")) {
+			inSpace = true;
+			otherTag = other.gameObject.tag;
+		}
+
+
+
+	
+	}
+
 }
